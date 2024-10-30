b03a5c9a6f093af50b5329fb194b0ec9d0ae1057
Petra <petra@behemot.biz>
2024-10-30 08:30:19 +0100

    Update settings.py, set debug=True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,8 +62,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = True
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+# DEBUG = 'DEV' in os.environ
 
 # ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 ALLOWED_HOSTS = ['127.0.0.1',  os.environ.get('ALLOWED_HOST')]
