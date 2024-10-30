101f77e7666e84680aa05369113fcabd09a182b5
Petra <petra@behemot.biz>
2024-10-30 08:48:42 +0100

    Update settings, change HOST to HOSTS os.environ.get('ALLOWED_HOSTS')

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,7 +66,7 @@ DEBUG = True
 # DEBUG = 'DEV' in os.environ
 
 # ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-ALLOWED_HOSTS = ['127.0.0.1',  os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['127.0.0.1',  os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
