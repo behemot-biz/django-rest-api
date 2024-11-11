876f92d15f4a0ff180bc314ad51950c992e1eae6
Petra <petra@behemot.biz>
2024-11-11 18:55:24 +0100

    Adjust indentation

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,6 +4,10 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -12,15 +16,15 @@ class CommentSerializer(serializers.ModelSerializer):
     updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
-            request = self.context['request']
-            return request.user == obj.owner
-    
+        request = self.context['request']
+        return request.user == obj.owner
+
     def get_created_at(self, obj):
         return naturaltime(obj.created_at)
 
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
-    
+
     class Meta:
         model = Comment
         fields = [
@@ -30,4 +34,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
     post = serializers.ReadOnlyField(source='post.id')
